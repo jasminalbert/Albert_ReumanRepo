049ad35ed0a1fcdc7ca914fe94e0c154b5a172fc
Jasmin Albert <jasminalbert@Jasmins-MacBook-Air.local>
2020-04-15 20:13:10 -0500

    function for left tail noise

--- ModTailAssociatedNoise.R
+++ ModTailAssociatedNoise.R
@@ -1,10 +1,9 @@
 #function for making left-tail and right tail associated noise, B and B.sharp
-library(MASS)
 library(copula)
 
 source("lottery_normcop.R")
 
-AssTailMod <- function(mu.B, sigma.B, rho, totT, lefttail = TRUE){
+LeftTailMod <- function(mu.B, sigma.B, rho, totT){
 	
 	ccop <- claytonCopula(param = 1.05, dim = 2)
 	logB_pre <- rCopula(totT, ccop)
@@ -13,8 +12,11 @@ AssTailMod <- function(mu.B, sigma.B, rho, totT, lefttail = TRUE){
 	logB[,1] <- qnorm(logB_pre[,1], mean = mu.B[1], sd = 		sigma.B[1])
 	logB[,2] <- qnorm(logB_pre[,2], mean = mu.B[2], sd = 		sigma.B[2])
 	
-	return(logB)
+	return(exp(logB))
 	
 }
+
+Belft <- LeftTailMod(mu.B = mu.B, sigma.B = sigma.B, totT = totT)
+
 
 
