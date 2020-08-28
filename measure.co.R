2b0c3eb3bf70e911ea75eba0b813a4aa11172d05
Jasmin Albert <jasminalbert@24-124-102-179-static.midco.net>
2020-08-28 07:59:42 -0500

    edit

--- measure.co.R
+++ measure.co.R
@@ -13,7 +13,7 @@ source("./pop.sim_function.R")
 # N1 = invader initial abundance
 # dom = dominance threshold 
 
-measure.co <- function(time, d, sigma, mu1, mu2, N, N1, dom){
+measure.co <- function(time, d, sigma, mu1, mu2, N, N1, dom, hist=FALSE){
 	
 	#make noise
 	noise <-get_noise(mn=c(mu1,mu2),sdev=c(sigma,sigma),n=time,check = F)
@@ -26,12 +26,20 @@ measure.co <- function(time, d, sigma, mu1, mu2, N, N1, dom){
 	coexist <- lapply(pop, function(X){X[,1] < dom*N & X[,1] > (1-dom)*N})
 	RLE <- lapply(coexist, rle)
 	co_periods <- lapply(RLE, function(X){X$lengths[X$values == TRUE]})
-	lapply(co_periods, function(X){hist(X, main="histogram of coexistence period length")})
+	
+	#histogram
+	if (hist)
+	{
+		maxlist <- lapply(co_periods,max)
+		maxval <- max(unlist(maxlist))
+		lapply(co_periods, function(X){hist(X, main="histogram of coexistence period length", breaks=10, xlim=c(0,maxval))})
+	}
 	
 	#fraction of time where coexistence was possible
 	return(lapply(co_periods, function(X){sum(X)/time}))
 }
 
 #test
-set.seed(1223)
-measure.co(time=10000, d=1, sigma=1.6, mu1=0.1, mu2=0.1, N=50, N1=25, dom=.999)
+#set.seed(1223)
+#par(mfrow=c(1,1))
+#measure.co(time=100000, d=1, sigma=6.4, mu1=0.5, mu2=0.1, N=50, N1=25, dom=.9)
