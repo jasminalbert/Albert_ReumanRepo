b221c0c6e179f8ffa3740d7f11f6e2b943e5e6e8
Jasmin Albert <jasminalbert@24-124-102-179-static.midco.net>
2020-10-04 13:28:00 -0500

    more commenting

--- param_sim.R
+++ param_sim.R
@@ -1,31 +1,50 @@
 source("./NoiseAndSE.R")
-#This is code for running through the storage effect calculations for various sets of parameters as set below 
+#This is code for running through the storage effect calculations for various sets of parameters as set below a number of x times
 #Parameters include delta (death rate), mn1 and mn2 (mean birth rate of sp1 and sp2) and sdev (standard deviation of birth rate; equal for both species)
 
-delta <- seq(0,1,0.1)
-mn1 <- seq(.1,.9,.1)
-mn2 <- seq(.1,.9,.1)
-sdev <- c(.4,.8,1.6, 3.2,6.4)
+
+delta <- seq(0,1,0.1) #0 to 1 by 0.1
+mn1 <- seq(.1,.9,.1) #0.1 to 0.9 by 0.1
+mn2 <- seq(.1,.9,.1) #0.1 to 0.9 by 0.1
+sdev <- c(.4,.8,1.6, 3.2,6.4) #doubling from 0.4 to 6.4
+x <- 10 #number of run repeats
 
 #noise_SE(mn1, mn2, sdev, n, delta)
-
+# ^the function we will use for this run; a function that generates noise and then calculates SE
 
 
 tot_sims <- length(sdev)*length(mn1)*length(mn2)*length(delta)
+# for above paramters, there will be 4455 unique runs
 
+
+#Structure for nest: sdev<mn1<mn2<delta
+
+#organize parameters to follow nest structure from terminals inward and keep with length of tot_sims
 
 delta_sims <- rep(delta, tot_sims/length(delta))
+#repeat delta to fill tot_sims
+
 mn2_sims <- sort(rep(mn2, length(delta)))
+#repeat one mn2 value for every value of delta
+
 mn1_sims <- sort(rep(mn1, length(mn2_sims)))
+#repeat one mn1 value for every unique combination of mn2 and delta
+
 sdev_sims <- sort(rep(sdev, length(mn1_sims)))
+#repeat one sdev value for every unique combination of mn1, mn2, and delta 
+
+#these vectors will be used to fill columns of dataframe tot_sims long
+#vectors that did not reach tot_sims length will automatically repeat to reach that length
 
 
+#for data storage: a list of dataframes, each dataframe is a run repeat
 
-
-
-dflist <- vector(mode = 'list', length = 10)
+dflist <- vector(mode = 'list', length = x)
 
 simdfx <- data.frame(sdev=sdev_sims, mn1=mn1_sims, mn2=mn2_sims, delta=delta_sims, ELT=NA, sym=NA, ERT=NA)
+#data.frame with columns specifying the parameter values and columns to be filled with storage effect for ELT, sym, ERT
+
+#fill dflist with x number fo simdfx
 
 for (m in 1:length(dflist)){
 	for (i in 1:dim(simdfx)[1]) {
