ac98f8b3c027e7ce5c9413491d032a9625e33560
jasmin <pimsupa_jasmin@ku.edu>
2021-06-09 18:08:26 -0500

    added one comment

--- MASTER.R
+++ MASTER.R
@@ -4,6 +4,6 @@ source("./makenoise.R") #make standard noise, change M here
 
 source("./parameters.R") #set parameters sigma, mu1, mu2, delta
 
-source("./parameter_loop.R") #loop through parameters to get results
+source("./parameter_loop.R") #loop through parameters to get results #see section 4
 
 
