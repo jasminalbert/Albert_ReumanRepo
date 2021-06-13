aa44b8abf39cec486cdcab5ae93700fa63d9b718
jasmin <pimsupa_jasmin@ku.edu>
2021-06-13 15:15:43 -0500

    adding to function

--- plotr1_fxn.R
+++ plotr1_fxn.R
@@ -1,38 +1,77 @@
-plot_r1 <- function(sigma, mu, delta){
+plotdis <- function(b1,b2,...){
+  plot(b2, b1, ylab='b1', xlab="b2", pch=16, col="grey",...)
+  rangeb1 <- round(range(b1))
+  means <- round(c(mean(b1), mean(b2)),1)
+  u <- seq(rangeb1[1],rangeb1[2],0.1)
+  v <- u + means[2]-means[1]
+  lines(v,u, lty=3)
+  points(b2[b1>b2],b1[b1>b2], pch=21, col="red", bg="grey")
+}
+
+plot_r1 <- function(sigma, mu, delta, plottype){
   load("./noise_etc.RData")
   
-  b_l1 <- sigma*b_tilde$l[,1] + mu[1]
-  b_l2 <- sigma*b_tilde$l[,2] + mu[2]
-  r1_l <- r(b_l1-b_l2, delta)
-  
-  b_r1 <- sigma*b_tilde$r[,1] + mu[1]
-  b_r2 <- sigma*b_tilde$r[,2] + mu[2]
-  r1_r <- r(b_r1-b_r2, delta)
-  
+  n <- transform(b_tilde, u_tilde, rho, sigma, mu)
   b_s1 <- sigma*b_tilde$s[,1] + mu[1]
   b_s2 <- sigma*b_tilde$s[,2] + mu[2]
-  r1_s <- r(b_s1-b_s2, delta)
+  
+  l <- r_mets(n$b_l1-n$b_l2, delta, M)
+  r <- r_mets(n$b_r1-n$b_r2, delta, M)
+  s <- r_mets(n$u_s, delta, M)
+  sharp1 <- r_mets(n$u_1, delta, M)
+  sharp2 <- r_mets(n$u_2, delta, M)
+
+  DeltaI <- c(l$rbar_hat - sharp1$rbar_hat + sharp2$rbar_hat, 
+              r$rbar_hat - sharp1$rbar_hat + sharp2$rbar_hat,
+              s$rbar_hat - sharp1$rbar_hat + sharp2$rbar_hat)
+  
+  invPrb <- c(length(n$b_l1[n$b_l1>n$b_l2])/M, 
+              length(n$b_r1[n$b_r1>n$b_r2])/M, 
+              length(n$u_s[n$u_s>0])/M)
+  
+  invE <- c(mean(l$r[l$r>0]),
+            mean(r$r[r$r>0]),
+            mean(s$r[s$r>0]))
   
   #pdf("r1.pdf")
   par(oma=c(2,1,1,1), mfrow=c(3,1), mar=c(2,3,2,1))
   
+  if (plottype==1){
+    
+    plot(l[1:100], type='l', ylab='r1(L)',xlab=NULL, col='blue')
+    abline(h=mean(l), lty=3, col='red')
+    points(n$b_l1[1:100]-n$b_l2[1:100], cex=0.75)
+    mtext(paste("sd=", round(sd(l),4), "mean=", round(mean(l),4)), 1, cex=0.5, line=-1)
+    
+    plot(r[1:100], type='l', ylab='r1(R)',xlab=NULL, col='blue')
+    abline(h=mean(r), lty=3, col='red')
+    points(n$b_r1[1:100]-n$b_r2[1:100], cex=0.75)
+    mtext(paste("sd=", round(sd(r),4), "mean=", round(mean(r),4)), 1, cex=0.5, line=-1)
+    
+    plot(s[1:100], type='l', ylab='r1(S)',xlab=NULL, col='blue')
+    abline(h=mean(s), lty=3, col='red')
+    points(b_s1[1:100]-b_s2[1:100], cex=0.75)
+    mtext(paste("sd=", round(sd(s),4), "mean=", round(mean(s),4)), 1, cex=0.5,line=-1)
+    
+    mtext("timestep",1,outer=T, cex=.75)
+  }
   
-  plot(r1_l[1:100], type='l', ylab='r1(L)',xlab=NULL, col='blue')
-  abline(h=mean(r1_l), lty=3, col='red')
-  points(b_l1[1:100]-b_l2[1:100], cex=0.75)
-  mtext(paste("sd=", round(sd(r1_l),4), "mean=", round(mean(r1_l),4)), 1, cex=0.5, line=-1)
+
+  if (plottype==2){
+    
+    plotdis(n$b_l1[1:750],n$b_l2[1:750], main="left tail asymmetry")
+    mtext(paste("(L)  P[r1>0]=", round(invPrb[1],3), "E(r1|r1>0)=", round(invE[1],3)), 1, cex=0.5, line=-1)
+    
+    plotdis(n$b_r1[1:750],n$b_r2[1:750], main="right tail asymmetry")
+    mtext(paste("(R)  P[r1>0]=", round(invPrb[2],3), "E(r1|r1>0)=", round(invE[2],3)), 1, cex=0.5, line=-1)
+    
+    plotdis(b_s1[1:750],b_s2[1:750], main="symmetric")
+    mtext(paste("(S)  P[r1>0]=", round(invPrb[3],3), "E(r1|r1>0)=", round(invE[3],3)), 1, cex=0.5, line=-1)
+    
+  }
   
-  plot(r1_r[1:100], type='l', ylab='r1(R)',xlab=NULL, col='blue')
-  abline(h=mean(r1_l), lty=3, col='red')
-  points(b_r1[1:100]-b_r2[1:100], cex=0.75)
-  mtext(paste("sd=", round(sd(r1_r),4), "mean=", round(mean(r1_r),4)), 1, cex=0.5, line=-1)
-  
-  plot(r1_s[1:100], type='l', ylab='r1(S)',xlab=NULL, col='blue')
-  abline(h=mean(r1_l), lty=3, col='red')
-  points(b_s1[1:100]-b_s2[1:100], cex=0.75)
-  mtext(paste("sd=", round(sd(r1_s),4), "mean=", round(mean(r1_s),4)), 1, cex=0.5,line=-1)
   
   mtext(paste("sigma=", sigma, "mu=", mu[1],mu[2], "delta=", delta), 3,outer=T, cex=.75, line=-1)
-  mtext("timestep",1,outer=T, cex=.75)
+  
   #dev.off()
 }
