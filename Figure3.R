7733f3a302d3883d8979099d443b84c9293d1c36
jasmin <pimsupa_jasmin@ku.edu>
2021-08-12 16:50:25 -0500

    matching configs of 3 and 4

--- Figure3.R
+++ Figure3.R
@@ -1,19 +1,19 @@
 load("./params.RData")
 load("./noise_etc.RData")
 source("./decomp1_plotting.R")
-base <- par()
-
-#pdf("fig3.pdf")
-
-#par(mfrow=c(1,1), mar = c(1,2,1,1), oma=c(1.5,3,3,1))
 
 
-#layout(matrix(c(1,2,3,4,5,
-#                6,10,11,12,13,	
-#                7,14,15,16,17,
-#                8,18,19,20,21,
-#                9,22,23,24,25), ncol=5, byrow=TRUE), 
-#       heights=c(1,3,3,3,3), widths=c(1,2.5,2.5,2.5,2.5))
+pdf("fig3_legend.pdf")
+
+par(mgp=c(3,0.5,0), mar = c(1,1,1,1), oma=c(3,5,3,2), xpd=TRUE)
+
+
+layout(matrix(c(2,3,4,5,1,26,
+                10,11,12,13,6,26,	
+                14,15,16,17,7,26,
+                18,19,20,21,8,26,
+                22,23,24,25,9,26), ncol=6, byrow=TRUE), 
+       heights=c(1,3,3,3,3), widths=c(2.5,2.5,2.5,2.5,1.5,2))
 #layout.show(n=25)
 
 #plot(0, bty="n")
@@ -22,7 +22,7 @@ plot.new() #1
 #2-5
 for (d in 1:length(delta)){
   plot.new()
-  text(0.5,0.5,labels=paste(delta[d]),font=2)
+  text(0.5,0.3,labels=paste(delta[d]),font=2)
 }
 
 #6-9
@@ -33,28 +33,31 @@ for (m in 1:length(mudif)){
 
 #10-25
 res <- vector(mode='list',length=1)
+n <- 1
 for (m in 1:length(mudif)){
   for (d in 1:length(delta)){
     res <- append(res,dePlot1(mudif[m], delta[d], xaxt="n"))
-    axis(1, labels=FALSE, tick=TRUE)
+    axis(1, labels=ifelse(n>12, yes=TRUE, no=FALSE), tick=TRUE)
+    n <- n+1
   }
 }
 
-title(ylab=expression(mu[1]-mu[2]), outer=TRUE, line=0, font.lab=2, cex.lab=1.5)
-title(xlab=expression(delta), outer=TRUE, line=-50, font.lab=2, cex.lab=1.5)
-title(xlab=expression(sigma), outer=TRUE, line=0, cex.lab=1.3)
+mtext("contribution to coexistence", side=2, outer=TRUE, line=1.5, font=2, cex=1, at=0.46)
+mtext(expression(mu[1]-mu[2]), side=4, outer=TRUE, line=-8, font=2, cex=1.5, at=0.46)
+mtext(expression(delta), side=3, outer=TRUE, line=-0.5, font=2, cex=1.5, at=0.37)
+mtext(expression(sigma), outer=TRUE, side=1, line=0.5, cex.lab=1.3, at=0.37)
 
 #layout(matrix(c(1)))
 plot.new() #1
-legend("center", 
+legend("topright", 
        legend=c(expression(Delta[0]), expression(Delta[E]),
                 expression(Delta[C]), expression(Delta[("E#C")]),
                 expression(Delta["[EC]"]), expression(Delta["[E||C]"])
                 ,expression(r)), 
        col = c("black","black","black","black","blue","red","orange"),
-       lty = c(1,2,4,3,1,1,1), bty="n", cex=2)
+       lty = c(1,2,4,3,1,1,1), bty="n", cex=1.5, inset=c(0,0))
 
-#dev.off()
-res <- res[-1]
+dev.off()
+#res <- res[-1]
 
 
--- Figure4.R
+++ Figure4.R
@@ -1,26 +1,22 @@
-#source("./decomp2_plotting.R")
+source("./decomp2_plotting.R")
 
 delta <- c(1, 0.8, 0.5)
 sigma <- c(2, 4 ,5,6)
 
-pdf("fig4_samexaxis50.pdf")
+pdf("fig4_samexaxis50legend.pdf")
 
-par(mfrow=c(1,1), mar = c(1,2,1,1), oma=c(3,2,3,3))
+par(mgp=c(3,0.5,0), mar = c(1,1,1,1), oma=c(3,5,3,2), xpd=TRUE)
 
-layout(matrix(c(1,2,3,4,
-                5,9,10,11,	
-                6,12,13,14,
-                7,15,16,17,
-                8,18,19,20), ncol=4, byrow=TRUE), 
-       heights=c(1,3,3,3,3), widths=c(1,2.5,2.5,2.5))
+layout(matrix(c(2,3,4,1,21,
+                9,10,11,5,21,	
+                12,13,14,6,21,
+                15,16,17,7,21,
+                18,19,20,8,21), ncol=5, byrow=TRUE), 
+       heights=c(1,3,3,3,3), widths=c(2.5,2.5,2.5,1,2))
 
 
 plot.new() #1
-legend("center", 
-       legend=c(expression(r), expression(Delta["[E||C]"])
-                ,expression(r-Delta["[E||C]"])), 
-       col = c("orange","red","navy"),
-       lty = c(1,1,5), bty="n", cex=0.6)
+
 
 #2-4
 for (d in 1:length(delta)){
@@ -31,7 +27,7 @@ for (d in 1:length(delta)){
 #5-8
 for (s in 1:length(sigma)){
   plot.new()
-  text(0.8,0.5,labels=paste(sigma[s]), font=2)
+  text(0.3,0.5,labels=paste(sigma[s]), font=2)
 }
 
 #9-20
@@ -40,16 +36,21 @@ m <- 1
 for (s in 1:length(sigma)){
   for (d in 1:length(delta)){
     res <- append(res,dePlot2(mudif_list[[m]],sigma[s], delta[d], xaxt="n"))
-    axis(1, labels=ifelse(m%%3==0, yes=TRUE, no=FALSE), tick=TRUE)
+    axis(1, labels=ifelse(m>9, yes=TRUE, no=FALSE), tick=TRUE)
     m <- m+1
   }
 }
 
-title(ylab="contribution to coexistence", outer=TRUE, line=-2.5, font.lab=2, cex.lab=1.5)
-title(ylab=expression(sigma), outer=TRUE, line=-30, font.lab=2, cex.lab=1.5)
-title(xlab=expression(delta), outer=TRUE, line=-48, font.lab=2, cex.lab=1.5)
-title(xlab=expression(mu[1]-mu[2]), outer=TRUE, line=1.4, cex.lab=1.3)
+mtext("contribution to coexistence", side=2, outer=TRUE, line=1.5, font=2, cex=1, at=0.46)
+mtext(expression(sigma), side=4, outer=TRUE, line=-8, font=2, cex=1.5, at=0.46)
+mtext(expression(delta), side=3, outer=TRUE, line=-0.5, font=2, cex=1.5, at=0.476)
+mtext(expression(mu[1]-mu[2]), outer=TRUE, side=1, line=0.5, cex.lab=1.3, at=0.476)
 
-
+plot.new() #21
+legend("topright", 
+       legend=c(expression(r), expression(Delta["[E||C]"])
+                ,expression(r-Delta["[E||C]"])), 
+       col = c("orange","red","navy"),
+       lty = c(1,1,5), bty="n", cex=1.5, inset=c(0,0))
 
 dev.off()
